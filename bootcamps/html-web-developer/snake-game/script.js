aaacfc67ae51941bd7467cc5328a6046cbfa3600
Waldener Monteiro <jjrr019@gmail.com>
2021-01-13 09:24:20 -0300

    feature: iniciando o projeto snake game e impedindo que a snake suma

---
+++ bootcamps/html-web-developer/snake-game/script.js
@@ -0,0 +1,56 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+
+let box = 32;
+let snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box,
+};
+let direction = "right";
+
+function createBG() {
+  context.fillStyle = "lightgreen";
+  context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function createSnake() {
+  for (let index = 0; index < snake.length; index++) {
+    context.fillStyle = "green";
+    context.fillRect(snake[index].x, snake[index].y, box, box);
+  }
+}
+document.addEventListener("keydown", update);
+
+function update(event) {
+  if (event.keyCode == 37 && direction != "right") direction = "left";
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  if (event.keyCode == 39 && direction != "left") direction = "right";
+  if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+function startGame() {
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+  createBG();
+  createSnake();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction === "right") snakeX += box;
+  if (direction === "left") snakeX -= box;
+  if (direction === "up") snakeY -= box;
+  if (direction === "down") snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+
+  snake.unshift(newHead);
+}
+let jogo = setInterval(startGame, 100);
